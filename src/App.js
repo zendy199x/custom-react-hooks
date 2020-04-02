daba1cbbe847d2611e8e6973be5a986a16634e30
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-02 18:18:07 +0700

    useState()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./App.scss"
+import ColorBox from "./components/ColorBox"
 
 function App() {
 	return (
 		<div className="app">
 			<h1>React Hooks</h1>
+			<ColorBox />
 		</div>
 	)
 }
