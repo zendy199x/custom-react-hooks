fa642154e7dba356a53fad6d05a06e0c67626fad
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-14 02:08:58 +0700

    setup file redux project

--- project/redux-photo-project/src/App.js
+++ project/redux-photo-project/src/App.js
@@ -1,5 +1,5 @@
 import React, {Suspense} from "react"
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
+import {BrowserRouter, Route, Switch, Redirect, Link} from "react-router-dom"
 import "./App.scss"
 import Header from "./components/Header"
 import NotFound from "./components/NotFound"
@@ -13,6 +13,17 @@ function App() {
 			<Suspense fallback={<div>Loading...</div>}>
 				<BrowserRouter>
 					<Header />
+					<ul>
+						<li>
+							<Link to="/photos">Go to photo page</Link>
+						</li>
+						<li>
+							<Link to="/photos/add">Go to add new photo page</Link>
+						</li>
+						<li>
+							<Link to="/photos/123">Go to edit photo page</Link>
+						</li>
+					</ul>
 
 					<Switch>
 						<Redirect exact from="/" to="/photos" />
