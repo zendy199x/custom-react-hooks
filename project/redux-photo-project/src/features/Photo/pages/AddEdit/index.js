02a7806fedfa90e4d91c197a4f61235977d86425
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-15 17:28:13 +0700

    setup reactstrap UI

--- project/redux-photo-project/src/features/Photo/pages/AddEdit/index.js
+++ project/redux-photo-project/src/features/Photo/pages/AddEdit/index.js
@@ -1,9 +1,22 @@
 import React from "react"
+import Banner from "../../../../components/Banner"
+import PhotoForm from "../../components/PhotoForm"
+import "./styles.scss"
 
 AddEditPage.propTypes = {}
 
 function AddEditPage(props) {
-	return <div>Photo Add Edit Page</div>
+	return (
+		<div className="photo-edit">
+			<Banner title="Pick your amazing photo 😎" />
+
+			<div className="photo-edit__form">
+				<PhotoForm
+					onSubmit={(values) => console.log("Form submit: ", values)}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default AddEditPage
--- project/redux-photo-project/src/features/Photo/pages/Main/index.js
+++ project/redux-photo-project/src/features/Photo/pages/Main/index.js
@@ -1,9 +1,21 @@
 import React from "react"
+import Banner from "../../../../components/Banner"
+import Images from "../../../../constants/images"
+import {Link} from "react-router-dom"
+import {Container} from "reactstrap"
 
 MainPage.propTypes = {}
 
 function MainPage(props) {
-	return <div>Photo Main Page</div>
+	return (
+		<div>
+			<Banner title="You awesome photos" backgroundUrl={Images.PINK_BG} />
+
+			<Container className="text-center">
+				<Link to="/photos/add">Add new photo</Link>
+			</Container>
+		</div>
+	)
 }
 
 export default MainPage
